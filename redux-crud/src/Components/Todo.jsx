bb2222e23b6cc4752d821d953f8e9f78765c17dc
Amritpal Bhinder <bhinderrajwinderkaur407@gmail.com>
2025-08-11 09:45:05 +0530

    Add files via upload

---
+++ redux-crud/src/Components/Todo.jsx
@@ -0,0 +1,38 @@
+
+import React, { use } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { add, remove, update } from "./Features/todoSlice.js";
+
+export default function Todos() {
+  const todos = useSelector((state) => state.todo.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {todos.map((todo, index) => {
+        return (
+          <p key={index}>
+            {todo.title} - {todo.status ? "Completed" : "In Completed"}
+            <button onClick={() => dispatch(remove(index))}>X</button>
+            <button
+              onClick={() =>
+                dispatch(
+                  update({
+                    index: index,
+                    title: "updated title",
+                    status: true,
+                  })
+                )
+              }
+            >
+              Edit
+            </button>
+          </p>
+        );
+      })}
+
+      <button onClick={() => dispatch(add("New Task - 10am"))}>Add Todo</button>
+    </div>
+  );
+}
+
